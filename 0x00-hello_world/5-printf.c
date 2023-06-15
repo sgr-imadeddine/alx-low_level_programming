473e4736b810fafb18611d06d1d45d30de6b9216
Your Name <you@example.com>
2023-06-15 14:28:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -  starting point for program execution
+*Return: always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	retutn(0);
+}
