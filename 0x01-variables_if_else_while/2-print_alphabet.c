bb03793166b1555737ee5bafd26d81669cfaee72
Your Name <you@example.com>
2023-06-16 17:22:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - starting point
+*description:  the alphabet in lowercase
+* Return: 0
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'a' ; n <= 'z' ; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
