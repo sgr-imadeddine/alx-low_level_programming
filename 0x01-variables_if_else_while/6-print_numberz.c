89ff782049ff6d14fdc5f4703e8e1e53698387ce
Your Name <you@example.com>
2023-06-16 18:18:40 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - starting point
+*description:  the numbers 0 to 9 method 2
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
