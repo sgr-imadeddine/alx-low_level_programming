3fa77b11c2067d55456ee43898e8b4c659ebfa58
Your Name <you@example.com>
2023-06-20 14:19:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet - function that prints the alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+int a;
+
+for (a = 97; a <= 122; a++)
+{
+	_putchar(a);
+}
+	_putchar('\n');
+}
