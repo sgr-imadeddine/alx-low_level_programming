a994854277b2a8b9b05ab37b25a5984d1839cb89
Your Name <you@example.com>
2023-07-12 19:10:55 -0700

    all in one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - prints the content of all args
+*
+*@argc: argument count
+*@argv: argument vector
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+	return (0);
+}
